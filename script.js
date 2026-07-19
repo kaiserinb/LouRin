f3a70f75f63a445b5be2b9e600e178e13b12b2bb
kaiserinb <kaiserinjoygardose@gmail.com>
2026-07-19 17:33:40 +0800

    Version 0.1.2 - Navigation

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+/*
+Version: 0.1.2
+File: script.js
+Project: LouRin
+*/
 
+const loadingScreen = document.getElementById("loadingScreen");
+const app = document.getElementById("app");
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        loadingScreen.style.display = "none";
+        app.style.display = "block";
+    }, 2500);
+});
+
+function showPage(pageId){
+
+    const pages = document.querySelectorAll(".page");
+
+    pages.forEach(page=>{
+        page.classList.remove("active");
+    });
+
+    document
+        .getElementById(pageId)
+        .classList.add("active");
+
+}
