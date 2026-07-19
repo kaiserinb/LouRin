f58d8859c278636bfaa0ec43d68c23230b93005e
kaiserinb <kaiserinjoygardose@gmail.com>
2026-07-19 22:15:46 +0800

    Refactor Firebase setup and expose db as firebaseDB

--- firebase.js
+++ firebase.js
@@ -1,25 +1,54 @@
 // Firebase Setup
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
 import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+getFirestore,
+collection,
+addDoc,
+query,
+orderBy,
+onSnapshot,
+serverTimestamp
+}
+from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDKp-oSIIb27t3bE2LpNVONa2cJsCwVnAE",
-    authDomain: "lourin-71b90.firebaseapp.com",
-    projectId: "lourin-71b90",
-    storageBucket: "lourin-71b90.firebasestorage.app",
-    messagingSenderId: "598113052882",
-    appId: "1:598113052882:web:a7d1d0bee66abb4d84e50b"
+
+apiKey: "AIzaSyDKp-oSIIb27t3bE2LpNVONa2cJsCwVnAE",
+
+authDomain: "lourin-71b90.firebaseapp.com",
+
+projectId: "lourin-71b90",
+
+storageBucket: "lourin-71b90.firebasestorage.app",
+
+messagingSenderId: "598113052882",
+
+appId: "1:598113052882:web:a7d1d0bee66abb4d84e50b"
+
 };
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
-// Make them available everywhere
-window.app = app;
-window.db = db;
+window.firebaseDB = db;
 
-console.log("❤️ Firebase Connected!");
+window.firebaseTools = {
+
+collection,
+
+addDoc,
+
+query,
+
+orderBy,
+
+onSnapshot,
+
+serverTimestamp
+
+};
+
+console.log("❤️ Firebase Ready");
